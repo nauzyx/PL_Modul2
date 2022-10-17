7412775e65cc3ceff21bf420122dfdc27ca260e1
nauzyx <yuandy7gamers@gmail.com>
2022-10-17 20:44:25 +0700

    PL: Modul2: Update Tugas1

---
+++ src/Tugas1Model.java
@@ -0,0 +1,42 @@
+public class Tugas1Model {
+    private String NIM, Email, Nama, Telp, Alamat;
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getTelp() {
+        return Telp;
+    }
+
+    public void setTelp(String telp) {
+        Telp = telp;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        Alamat = alamat;
+    }
+}
